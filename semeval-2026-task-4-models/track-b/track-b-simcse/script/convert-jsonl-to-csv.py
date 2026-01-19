a127d9a5606f2f922fa1f9109867283bb29a5505
Jianfei Xu <mr.xujianfei@gmail.com>
2026-01-19 12:46:48 +0000

    feat(ncl-simcse):fine-tuning simcse large model using task4 datasets

--- semeval-2026-task-4-models/track-b/track-b-simcse/script/convert-jsonl-to-csv.py
+++ semeval-2026-task-4-models/track-b/track-b-simcse/script/convert-jsonl-to-csv.py
@@ -1,6 +1,7 @@
 import json
 import csv
 
+
 def convert_jsonl_to_csv(input_jsonl_file, output_csv_file):
     with open(input_jsonl_file, "r", encoding="utf-8") as fin, \
          open(output_csv_file, "w", encoding="utf-8", newline="") as fout:
@@ -19,6 +20,10 @@ def convert_jsonl_to_csv(input_jsonl_file, output_csv_file):
             text_a = obj["text_a"]
             text_b = obj["text_b"]
 
+            if anchor is None or anchor == "":
+                print(line)
+                continue
+
             if obj["text_a_is_closer"]:
                 closer = text_a
                 farther = text_b
@@ -32,8 +37,8 @@ def convert_jsonl_to_csv(input_jsonl_file, output_csv_file):
 
 
 if __name__ == '__main__':
-    input_jsonl_file = "../../../../semeval-2026-task-4-datasets/semeval-2026-task-4-dev-v1/dev_track_a.jsonl"  # 你的jsonl文件
-    # input_jsonl_file = "../../../../semeval-2026-task-4-datasets/semeval-2026-task-4-train-v1/train_track_a.jsonl"  # 你的jsonl文件
-    output_csv_file = "../data/dev_track_a.csv"  # 输出csv文件
-    # output_csv_file = "train_track_a.csv"  # 输出csv文件
+    # input_jsonl_file = "../../../../semeval-2026-task-4-datasets/semeval-2026-task-4-dev-v1/dev_track_a.jsonl"  # 你的jsonl文件
+    input_jsonl_file = "../../../../semeval-2026-task-4-datasets/semeval-2026-task-4-train-v1/train_track_a.jsonl"  # 你的jsonl文件
+    # output_csv_file = "../data/dev_track_a.csv"  # 输出csv文件
+    output_csv_file = "../data/train_track_a.csv"  # 输出csv文件
     convert_jsonl_to_csv(input_jsonl_file, output_csv_file)
---
+++ semeval-2026-task-4-models/track-b/track-b-simcse/simcse/train-supervised-simcse-model.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# In this example, we show how to train SimCSE using multiple GPU cards and PyTorch's distributed data parallel on supervised NLI dataset.
+# Set how many GPUs to use
+
+NUM_GPU=1
+
+# Randomly set a port number
+# If you encounter "address already used" error, just run again or manually set an available port id.
+PORT_ID=$(expr $RANDOM + 1000)
+
+# Allow multiple threads
+export OMP_NUM_THREADS=8
+
+# Use distributed data parallel
+# If you only want to use one card, uncomment the following line and comment the line with "torch.distributed.launch"
+# python train.py \
+python -m torch.distributed.launch \
+    --nproc_per_node $NUM_GPU \
+    --master_port $PORT_ID "./train.py" \
+    --model_name_or_path "../sup-simcse-roberta-large" \
+    --train_file "../data/train_track_a.csv" \
+    --output_dir "../train/checkpoints/sup-simcse-roberta-large-v1" \
+    --num_train_epochs 5 \
+    --per_device_train_batch_size 32 \
+    --learning_rate 1e-5 \
+    --max_seq_length 32 \
+    --evaluation_strategy steps \
+    --metric_for_best_model stsb_spearman \
+    --load_best_model_at_end \
+    --eval_steps 60 \
+    --save_steps 60 \
+    --pooler_type cls \
+    --overwrite_output_dir \
+    --temp 0.05 \
+    --do_train \
+    --do_eval \
+    --hard_negative_weight 0.3 \
+    --logging_steps 10 \
+    --logging_dir "../train/checkpoints/sup-simcse-roberta-large-v1/runs" \
+    --mlp_only_train \
+#    --fp16 \
+    "$@"
